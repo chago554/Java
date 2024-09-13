410a7fc8bf17290522a69717bf1917bf320b3846
santiago <santiagojesuslaureano369@gmail.com>
2024-09-12 19:27:51 -0600

    Se agrega los metodos setters y los atributos private

--- src/desarrolloapps/DesarrolloApps.java
+++ src/desarrolloapps/DesarrolloApps.java
@@ -1,14 +1,31 @@
-
 package desarrolloapps;
 
+import java.util.Scanner;
 
 public class DesarrolloApps {
 
-  
+    public static Scanner teclado = new Scanner(System.in);
+    private double a;
+    private double b;
+    private double c;
+
+    private double resultado;
+
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Hola mundo");
-        
     }
-    
+
+    public void setA(double a) {
+        this.a = a;
+    }
+
+    public void setB(double b) {
+        this.b = b;
+    }
+
+    public void setC(double c) {
+        this.c = c;
+    }
+
 }
